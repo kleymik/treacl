8883254bea8cc708d75ad883cd42407ddb13a4b0
Mike Kleyn <michfkl@btinternet.com>
2020-05-10 23:12:33 +0100

    treacl: refining table example

--- examples/table.py
+++ examples/table.py
@@ -1,4 +1,3 @@
-
 # quirky use of a treacl dag to create a table
 
 from treacl import Treacl as t
@@ -9,45 +8,62 @@ def print_treacl_table(tbl):
         print()
     print()
 
+
 def make_treacl_3x2_table():
     '''example 3x2 table
     '''
     tbl = t()
-    tbl.rows = [ t(), t(), t()] # i.e. [ row1, row2, row3]
-    tbl.cols = [ t(), t() ]     # i.e. [ col1, col2 ]
-                                # put elements in row series (i.e. .s)
+    tbl.rows = [ t(), t(), t()] # i.e. [ row1, row2, row3] = list of row series
+    tbl.cols = [ t(), t() ]     # i.e. [ col1, col2 ]      # list of column series
+                                # put elements in row series (i.e. ".s")
     tbl.rows[0].s = [t(), t()]  # a b
     tbl.rows[1].s = [t(), t()]  # c d
     tbl.rows[2].s = [t(), t()]  # e f
                                 # put *same* elements in column series (i.e. ".s")
-    tbl.cols[0].s = [tbl.rows[0].s[0], tbl.rows[1].s[0], tbl.rows[2].s[0]] # [e1 e3 e5]
-    tbl.cols[1].s = [tbl.rows[0].s[1], tbl.rows[1].s[1], tbl.rows[2].s[1]] # [e2 e4 e6]
+    tbl.cols[0].s = [tbl.rows[0].s[0], tbl.rows[1].s[0], tbl.rows[2].s[0]] # [a c e]
+    tbl.cols[1].s = [tbl.rows[0].s[1], tbl.rows[1].s[1], tbl.rows[2].s[1]] # [b d f]
                                 # give each element in the table a value attribute (i.e.".v" )
-    tbl.rows[0].s[0].v = "a"
-    tbl.rows[0].s[1].v = "b"
-    tbl.rows[1].s[0].v = "c"
-    tbl.rows[1].s[1].v = "d"
-    tbl.rows[2].s[0].v = "e"
-    tbl.rows[2].s[1].v = "f"
+    tbl.rows[0].s[0].v = "a"; tbl.rows[0].s[1].v = "b"
+    tbl.rows[1].s[0].v = "c"; tbl.rows[1].s[1].v = "d"
+    tbl.rows[2].s[0].v = "e"; tbl.rows[2].s[1].v = "f"
 
     return tbl
+
 
 def make_treacl_table(nrows, ncols):
-    '''example nRxnC table
+    '''example nR X nC table
     '''
     tbl = t()
-    tbl.rows = [t() for i in range(nrows)]
-    tbl.cols = [t() for i in range(ncols)]
+    tbl.rows = [t() for i in range(nrows)] # list of row series
+    tbl.cols = [t() for i in range(ncols)] # list of column series
     for r in tbl.rows: r.s = [t() for x in tbl.cols]
-    for ci,c in enumerate(tbl.cols): c.s = [ t for t in tbl.rows[ci]]
-    # for bi,b in enumerate(tbl.cols): tbl.rows[ai].s[bi] = tbl.rows[bi].s[ai] = t()
-
+    cntr = 0
+    for ri,r in tbl.rows:
+        for ci,c in tbl.cols:
+            e = tbl.rows[ri].s[ci]
+            e.v = cntr
+            cntr += 1
+    for ci,c in enumerate(tbl.cols): c.s = [r.s[ci] for r in tbl.rows]
     return tbl
+
+
+def make_treacl_symmatrix(nrows, ncols):
+    '''a symmetric marix as a table: corresponding off-diagonal items
+       are not just equal (m[i,j]==m[j,i])==True, they are the
+       same "(m[i,j] is m[j,i])"==True.
+    '''
+    tbl = t()
+    tbl.rows = [t() for i in range(nrows)] # list of row series
+    tbl.cols = [t() for i in range(ncols)] # list of column series
+    for ri,r in tbl.rows:
+        for ci,c in enumerate(tbl.cols): tbl.rows[ri].s[ci] = tbl.cols[ci].s[ri] = t
+    return tbl
+    # for r in tbl.rows: r.s = [t() for x in tbl.cols]
+    # for ci,c in enumerate(tbl.cols): c.s = [ t for t in tbl.rows[ci]]
 
 if __name__ == '__main__':
 
     tb1 = make_treacl_3x2_table()
-
     print_treacl_table(tb1)
 
     # transpose
@@ -55,28 +71,23 @@ if __name__ == '__main__':
     print("transposed")
     print_treacl_table(tb1)
 
-
-#tbl.meta.rows[0].cell =
-
-# for a,ai in emuerate(tbl.meta.rows):
-#     for b,bi in enumerate(tbl.meta.cols):
-#         tbl.meta.rows[ai][bi] = tbl.meta.rows[bi][ai] = t()
-
-#         tbl.rows = [ r1, r2, r3]
-# tbl.cols = [ c1, c2 ]
-
-# r1.s = [e1 e2]
-# r2.s = [e3 e4]
-# r3.s = [e5 e6]
-
-# c1.s = [e1 e3 e5]
-# c2.s = [e2 e4 e6]
-
-# tbl.meta.rows, tbl.meta.cols = tbl.meta.cols, tbl.meta.rows
-
-# for a in tbl.meta.rows:
-#     for b in tbl.meta.cols:
-#         print a,b
+    # n x m table
+    tb2 = make_treacl_table(2,4)
+    print_treacl_table(tb2)
 
 
+# cntr = 0
+# for ri,r in enumerate(tbl.rows):
+#     tbl.rows[ri].s = [None,]*len(tbl.cols)
+#     for ci,c in enumerate(tbl.cols):
+#         newcell = t()
+#         newcell.v = cntr
+#         cntr += 1
+#         tbl.rows[ri].s[ci] = newcell
+#         # tbl.cols[ci].s[ri] = newcell
+# return tbl
 
+# for bi,b in enumerate(tbl.cols): tbl.rows[ai].s[bi] = tbl.rows[bi].s[ai] = t()
+# for bi,b in enumerate(tbl.cols): tbl.rows[ai].s[bi] = tbl.rows[bi].s[ai] = t()
+# for r in tbl.rows: r.s = [t() for x in tbl.cols]
+# for ci,c in enumerate(tbl.cols): c.s = [ t for t in tbl.rows[ci]]
