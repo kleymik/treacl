daa00fb7824053e23f317bb62dbbbac222f42e57
Mike Kleyn <michfkl@btinternet.com>
2020-05-13 20:23:15 +0100

    treacl: override getstate & setstate if you want to pickle; use walrus a bit more; move as yet unused methods out

--- treacl.py
+++ treacl.py
@@ -21,14 +21,17 @@ class Treacl(object):
         setattr(self, name, vdd := Treacl())                                              # I am the walrus
         return vdd
 
+    def __getstate__(self): return vars(self)                                             # otherwise pickle complains
+    def __setstate__(self, state): vars(self).update(state)
+
 
     # Treacl "user" properties                                                            # as an alternative to attributes in the dunder .__dict__
                                                                                           # see README explanation
     __props = {}
 
     def __init__(self):
-        self.__props = {"long_name":None,
-                          "type"   :None }
+        self.__props = {"long_name": None,
+                        "type"     : None }
 
     def addProp(self, pName, value):
         self.__props[pName] = value
@@ -47,39 +50,37 @@ class Treacl(object):
 
     # some basic traversal methods
 
-    treeMaxDepth = 10_000_000                                                             # in case of cycles: to limit searches & traversals
+    treeMaxDepth = 100                                                             # in case of cycles: to limit searches & traversals
     depthIndent = 4                                                                       # indent for tree printing
 
     def pf_indented(self, v, indent=0):
-        """use pprint pformat but then addtional indent for given depth
-        """                                                                               # (pprint module inserts one less whitespace for first line)
-        pfStrings = pformat(v, width=20).split('\n')                                      # (indent=1 is default, giving everything one extra whitespace)
+        '''use pprint pformat but then addtional indent for given depth
+        '''                                                                               # (pprint module inserts one less whitespace for first line)
+        pfStrings = pformat(v, width=40).split('\n')                                      # (indent=1 is default, giving everything one extra whitespace)
         return [('' if num==0 else ' ' * indent) + line for num, line in enumerate(pfStrings)]
 
     def pptree(self, depth=0):
         '''print treacl tree recursively'''
         if depth < self.treeMaxDepth:
-            for at in vars(self):                                                             # same as self.__dict__: # tbd: if singleton, don't print a CRLF
-                if not at.startswith('_'):
-                    nameStr = ' ' * self.depthIndent * depth + f'{at}: '
-                    print(nameStr, end='')
-                    atv = getattr(self, at)                                                   # attribute value
-                    if isinstance(atv, Treacl):
-                        print()
-                        atv.pptree(depth + 1)                                                 # recurse
-                    elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]): # recurse into lists only (not any other iterables), if case they contain at leat one Treacl
-                        print()
-                        for e in atv:                                                         # note deeper nested lists are not checked
-                            if isinstance(e, Treacl): e.pptree(depth + 1)                     # recurse
-                            else:
-                                for s in self.pf_indented(e, depth=depth+1): print(s)         # use pretty print to print python base datatype
-                    else:
-                        for s in self.pf_indented(atv, len(nameStr)): print(s)                # tbd: keep indent even if multi-line
+            for at in [k for k in vars(self).keys() if not k.startswith('_')]:            # same as self.__dict__: # tbd: if singleton, don't print a CRLF
+                print(nameStr := ' ' * self.depthIndent * depth + f'{at}: ', end='')
+                atv = getattr(self, at)                                                   # attribute value
+                if isinstance(atv, Treacl):
+                    print()
+                    atv.pptree(depth + 1)                                                 # recurse
+                elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]): # recurse into lists only (not any other iterables), if case they contain at leat one Treacl
+                    print()
+                    for e in atv:                                                         # note deeper nested lists are not checked
+                        if isinstance(e, Treacl): e.pptree(depth + 1)                     # recurse
+                        else:
+                            for s in self.pf_indented(e, depth=depth+1): print(s)         # use pretty print to print python base datatype
+                else:
+                    for s in self.pf_indented(atv, len(nameStr)): print(s)                # tbd: keep indent even if multi-line
         else:
-            print("Max recursion depth reached %i", treeMaxDepth)
+            print(f"Max recursion depth reached {self.treeMaxDepth}")
 
     def tprint(self, depth=0):
-        for at in vars(self):                                                             # tbd: if singleton, don't print a CRLF
+        for at in [k for k in vars(self).keys() if not k.startswith('_')]:                # tbd: if singleton, don't print a CRLF
             atv = getattr(self, at)                                                       # attribute value
             print('\n'+'Z' * self.depthIndent * depth, f'{at}: ', end='')
             treacl_pprint(atv, depth + 1)                                                 # recurse
@@ -87,10 +88,9 @@ class Treacl(object):
 
     def pathsToList(self, curPath="", resLst=[], depth=0, showValP=False):                # list all paths in tree
         '''generate all paths to a given depth'''
-        for at in vars(self):
+        for at in [k for k in vars(self).keys() if not k.startswith('_')]:
             if not at.startswith('_'):
-                pth = curPath+'.'+f'{at}'
-                resLst += [pth]
+                resLst += [pth := curPath+'.'+f'{at}']
                 if isinstance(atv := getattr(self, at), Treacl): atv.pathsToList(pth, resLst) # recurse
         return resLst
 
@@ -103,7 +103,7 @@ class Treacl(object):
         return [ p for p in self.pathsToList() if pattrn in p.split('.')]
 
 
-    def findPathsRegex(self, regexPattrn, caseSensitive=re.I, depth=0, showValP=False):   # list paths that match a pattern
+    def findPathsRegex(self, regexPattrn, caseSensitive=re.I, depth=0, showValP=False):   # list paths that match a regex pattern
         '''search tree recursively depth first
            find all paths with pattern matching
            anywhere in the path
@@ -112,89 +112,39 @@ class Treacl(object):
         return [ p for p in self.pathsToList() if lineRePat.search(p)]
 
 
-    def findPaths2(self, pattrn, depth=0, showVal=False):                                 # list paths that match a pattern
-        '''search tree recursively depth first
-           find all paths with pattern matching
-           anywhere in the path
-        '''
-        for at in vars(self):
-            atv = getattr(self, at)  # attribute value
-            if isinstance(atv, Treacl):
-                atv.pptree(depth + 2)  # recurse
-                if pattrn in str(atv):
-                    print(' ' * depth, f'{at}:')
-            else:
-                if pattrn in str(atv):
-                    print(' ' * depth, f'{at}=', end='')
-                    print(pformat(atv))
-
-
     # graph functions
 
     def ppgraph(self, depth=0, occurDict={}):
-        '''print treacl tree recursively'''
+        '''print treacl graph recursively'''
 
         print(' ' * self.depthIndent * depth+self.getProp("name"))
         occurDict[self] = True
         if depth < self.treeMaxDepth:
-            for at in vars(self):                                                      # tbd: if singleton, don't print a CRLF
-                if not at.startswith('_'):
-                    nameStr = ' ' * self.depthIndent * depth + f'{at}: '
-                    print(nameStr, end='')
-                    atv = getattr(self, at)                                                   # attribute value
-                    if isinstance(atv, Treacl):
-                        #print("*"*depth+"TREACL)")
-                        print()
-                        if atv not in occurDict:
-                            atv.ppgraph(depth + 1, occurDict=occurDict)                           # recurse
+            for at in [k for k in vars(self).keys() if not k.startswith('_')]:            # tbd: if singleton, don't print a CRLF
+                nameStr = ' ' * self.depthIndent * depth + f'{at}: '
+                print(nameStr, end='')
+                atv = getattr(self, at)                                                   # attribute value
+                if isinstance(atv, Treacl):
+                    print()
+                    if atv not in occurDict: atv.ppgraph(depth + 1, occurDict=occurDict)  # recurse
+                elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]): # recurse into lists only (not any other iterables), if case they contain at leat one Treacl
+                    print()
+                    for e in atv:                                                         # note deeper nested lists are not checked
+                        if isinstance(e, Treacl):
+                            if e not in occurDict: e.ppgraph(depth + 1, occurDict=occurDict) # recurse
                         else:
-                            # print("already1 visited node, ending recursion branch")
-                            pass
-                    elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]): # recurse into lists only (not any other iterables), if case they contain at leat one Treacl
-                        # print("*"*depth+"LIST")
-                        print()
-                        for e in atv:                                                         # note deeper nested lists are not checked
-                            if isinstance(e, Treacl):
-                                if e not in occurDict:
-                                    e.ppgraph(depth + 1, occurDict=occurDict)                     # recurse
-                                else:
-                                    # print("already2 visited node, ending recursion branch")
-                                    pass
-                            else:
-                                for s in self.pf_indented(e, depth=depth+1): print(s)         # use pretty print to print python base datatype
-                    else:
-                        for s in self.pf_indented(atv, len(nameStr)): print(s)                # tbd: keep indent even if multi-line
+                            for s in self.pf_indented(e, depth=depth+1): print(s)         # use pretty print to print python base datatype
+                else:
+                    for s in self.pf_indented(atv, len(nameStr)): print(s)                # tbd: keep indent even if multi-line
         else:
-            print("Max recursion depth reached %i", treeMaxDepth)
-        # print("*"*depth+"Exit")
+            print(f"Max recursion depth reached {treeMaxDepth}")
+
 
     # import / export graphs
 
     def paths_to_dot():
-        ''' export data as dot/dotty graph format file'''
+        ''' export data as dot/dotty graph format file
+            assumes all nodes are reachable from this point
+        '''
         return None
-
-# functions
-## cater for Treacl and Treacl inside other datatypes structures list
-
-treeMaxDepth = 10_000_000                                   # in case of cycles: limit any search
-depthIndent = 4                                             # indent for tree printing
-
-def treacl_pprint(node, depth=0):                            # not implemented as a method to allow pretty printer
-    '''print tree recursively'''                             # that recurses into non-treacl objects too
-    if isinstance(node, Treacl):
-        if depth < node.treeMaxDepth:
-            node.tprint(depth+1)
-            # print("AA")
-        else:
-            print("Max recursion depth reached %i", treeMaxDepth)
-    elif isinstance(node, str):
-        print(pformat(node))
-        # print("BB")
-    elif isinstance(node, Iterable):                    # but not a  string:
-        for e in node:
-            if isinstance(e, Treacl): node.tprint(depth + 1) # recurse
-            else:                     treacl_pprint(e, depth=depth+1)               # tbd: keep indent even if multi-line
-    else:
-        print(pformat(node), end='')
 
