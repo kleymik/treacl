f9c83fd115cc4319ca50fd20c40dd7d7e743a42f
Mike Kleyn <michfkl@btinternet.com>
2020-05-16 13:37:53 +0100

    treacl: tweak README

--- __init__.py
+++ __init__.py
@@ -1,45 +1,13 @@
-# make treacl a module
-
+# treacl
 
 __title__ = "treacl"
-__description__ = "tree class"
+__description__ = "tree and graphs via dynamic attributes"
 __author__  = 'kleymik'
 __version__ = '0.0.1'
-__copyright__ = 'Copyright (c) 2018-2020 Mike Kleyn'
+__copyright__ = 'Copyright (c) 2018 Mike Kleyn'
 __license__ = 'BSD License'
 
-# __version_info__ = VersionInfo._from_version_string(__version__)
-
-# __title__ = "attrs"
-# __description__ = "Classes Without Boilerplate"
-# __url__ = "https://www.attrs.org/"
-# __uri__ = __url__
-# __doc__ = __description__ + " <" + __uri__ + ">"
-
-# __author__ = "Hynek Schlawack"
-# __email__ = "hs@ox.cx"
-
-# __license__ = "MIT"
-# __copyright__ = "Copyright (c) 2015 Hynek Schlawack"
 
 
 
-# from see
 
-# """
-# see: dir for humans.
-
-# Documentation is available at https://ljcooke.github.io/see/
-
-# """
-# from .inspector import see
-# from .output import SeeResult
-
-
-# __all__ = ['see', 'SeeResult']
-
-# __author__ = 'Liam Cooke'
-# __contributors__ = 'See AUTHORS.rst'
-# __version__ = '1.4.1'
-# __copyright__ = 'Copyright (c) 2009-2017 Liam Cooke'
-# __license__ = 'BSD License'
