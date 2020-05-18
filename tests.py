e3bde6d909acf330447bd1a9295f43180b72f888
Mike Kleyn <michfkl@btinternet.com>
2020-05-18 15:01:33 +0100

    treacl: tests: tidy up a bit

--- tests.py
+++ tests.py
@@ -1,6 +1,36 @@
 import filecmp
 from contextlib import redirect_stdout
 
+if __name__ == '__main__':
+
+   test_scripts = [
+       "configuration",
+       "yaml",
+       "xml",
+       "json",
+       "universe",
+       "ssl_x509_cert",
+       "standard_model",
+       "table"
+   ]
+
+# "divide_by_7"
+# "mini_backprop"
+# "quantlib_instruments_hierarchy"
+
+   redirect = True
+   for ts in test_scripts:
+       print(f"Executing {ts} test")
+       if redirect:
+           with redirect_stdout(outfile := open(f"./tests/{ts}_test.out", 'w')):
+               exec(open(f"./examples/{ts}.py").read())
+           outfile.close()
+       else:
+           print("-"*100)
+           exec(open(f"./examples/{ts}.py").read())
+           print
+
+
 # import difflib
 # def difftest():
 #     import difflib
@@ -11,36 +41,6 @@ from contextlib import redirect_stdout
 #             # Find and print the diff:
 #             for line in difflib.unified_diff(f1_text, f2_text, fromfile='file1', tofile='file2', lineterm=''):
 #                 print line
-
-if __name__ == '__main__':
-
-   test_scripts = [
-       #configuration",
-       #yaml",
-       #xml",
-       #json",
-       "ssl_x509_cert"
-   ]
-#       "table",
-#       "standard_model",
-#       "divide_by_7"
-#       "mini_backprop"
-#       "quantlib_instruments_hierarchy"
-
-#       "universe"
-
-   for ts in test_scripts:
-       print(f"Executing {ts} test")
-       with redirect_stdout(outfile := open(f"./tests/{ts}_test.out", 'w')):
-           print("-"*100)
-           exec(open(f"./examples/{ts}.py").read())
-       outfile.close()
-
-       # if filecmp.cmp(f"./tests/{ts}_test.ref", f"./tests/{ts}_test.out"): print("TEST PASSED for: configuration")
-       # else:                                                            print("TEST FAILED for: configuration")
-
-
-
 
 
 
