6ca392e1d9c31ff66d23aaea4023d45d4ec8bd8c
Mike Kleyn <michfkl@btinternet.com>
2020-05-29 21:56:42 +0100

    got testing by diffing working

--- tests.py
+++ tests.py
@@ -1,51 +1,44 @@
 import filecmp
 from contextlib import redirect_stdout
+import sys
+import difflib
+from glob import glob
 
 if __name__ == '__main__':
 
-   test_scripts = [
-       "configuration",
-       "yaml",
-       "xml",
-       "json",
-       "universe",
-       "ssl_x509_cert",
-       "standard_model",
-       "table",
-       "divide_by_7"
-   ]
+   print(sys.argv)
+   for f in sorted(glob("./tests/*.ref")): print(f)
 
-   checkOuputDiffs = False
+   if sys.argv[1]=='all':
+       testScripts = ["configuration", "json", "ssl_x509_cert", "yaml", "xml"]
+       #testScripts = ["configuration", "yaml", "xml", "json", "universe", "ssl_x509_cert", "standard_model", "table", "divide_by_7"]
+       checkOuputDiffs = True
+   else:
+       testScripts = sys.argv[1:]
+       checkOuputDiffs = False
 
-   for ts in test_scripts:
+   for ts in testScripts:
+       print("-"*100, end='')
        print(f"Executing {ts} test")
-       if checkOuputDiffs:
-           with redirect_stdout(outfile := open(f"./tests/{ts}_test.out", 'w')):
+       if checkOuputDiffs:               # output to files
+           refOutFile = f"./tests/{ts}_test.ref"
+           newOutFile = f"./tests/{ts}_test.out"
+           with redirect_stdout(outfile := open(newOutFile, 'w')):
                exec(open(f"./examples/{ts}.py").read())
-           outfile.close()
-       else:
-           print("-"*100)
+               outfile.close()
+           with open(refOutFile) as f1: f1Text = f1.readlines()
+           with open(newOutFile) as f2: f2Text = f2.readlines()
+           numDiffLines = 0
+           for l in difflib.ndiff(f1Text, f2Text):
+               if l[0] in ['+', '-']:
+                   print(l, end='')
+                   numDiffLines +=1
+           print(f"{ts} test  num diffs = {numDiffLines//2}")
+       else:                             # to stdout
            exec(open(f"./examples/{ts}.py").read())
            print
 
-
-# import difflib
-# def difftest():
-#     import difflib
-#     with open('file1') as f1:
-#         f1_text = f1.read()
-#         with open('file2') as f2:
-#             f2_text = f2.read()
-#             # Find and print the diff:
-#             for line in difflib.unified_diff(f1_text, f2_text, fromfile='file1', tofile='file2', lineterm=''):
-#                 print line
-
-
-
-
-
-
-
+#    print("Done")
 
 
 
