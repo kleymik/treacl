873ae581bb0abcb42870572c8cc69f51628814f3
Mike Kleyn <michfkl@btinternet.com>
2020-06-02 13:52:05 +0100

    tidy tests runner

--- tests.py
+++ tests.py
@@ -4,14 +4,15 @@ import sys
 import difflib
 from glob import glob
 
+'''run each example and then do a diff on its output versus the tests/*.ref reference output'''
+
 if __name__ == '__main__':
 
    print(sys.argv)
    for f in sorted(glob("./tests/*.ref")): print(f)
 
    if sys.argv[1]=='all':
-       testScripts = ["configuration", "json", "ssl_x509_cert", "yaml", "xml"]
-       #testScripts = ["configuration", "yaml", "xml", "json", "universe", "ssl_x509_cert", "standard_model", "table", "divide_by_7"]
+       testScripts = ["configuration",  "divide_by_7", "json", "ssl_x509_cert", "standard_model", "table", "universe", "yaml", "xml"]
        checkOuputDiffs = True
    else:
        testScripts = sys.argv[1:]
@@ -26,19 +27,20 @@ if __name__ == '__main__':
            with redirect_stdout(outfile := open(newOutFile, 'w')):
                exec(open(f"./examples/{ts}.py").read())
                outfile.close()
-           with open(refOutFile) as f1: f1Text = f1.readlines()
-           with open(newOutFile) as f2: f2Text = f2.readlines()
+           with open(refOutFile) as fRef: refText = fRef.readlines()
+           with open(newOutFile) as fOut: outText = fOut.readlines()
            numDiffLines = 0
-           for l in difflib.ndiff(f1Text, f2Text):
+           for l in difflib.ndiff(refText, outText):
                if l[0] in ['+', '-']:
                    print(l, end='')
                    numDiffLines +=1
            print(f"{ts} test  num diffs = {numDiffLines//2}")
+           print()
+           print("Done")
        else:                             # to stdout
            exec(open(f"./examples/{ts}.py").read())
-           print
+           print()
 
-#    print("Done")
 
 
 
