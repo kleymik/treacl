f4976902b58b11a7ae68cb8b9d73ac8a582e5597
Mike Kleyn <michfkl@btinternet.com>
2020-08-17 16:27:04 +0100

    pdp: simple load/save using pickle, json exporter

--- treacl.py
+++ treacl.py
@@ -8,6 +8,8 @@
 import re
 import sys
 from pprint import pformat
+import pickle
+import json
 
 class Treacl(object):
     ''' Treacl: a tree class'''
@@ -24,26 +26,19 @@ class Treacl(object):
 
     def __setstate__(self, state): vars(self).update(state)
 
-
-    # def __getstate__(self):      # https://docs.python.org/3/library/pickle.html#handling-stateful-objects
-    #     """Return state values to be pickled."""
-    #     f = self.logfile
-    #     return (self.value, f.name, f.tell())
-
-    # def __setstate__(self, state):
-    #     """Restore state from the unpickled state values."""
-    #     self.value, name, position = state
-    #     f = file(name, 'w')
-    #     f.seek(position)
-    #     self.logfile = f
-
     def attrs_list(self, sortedP=False):
         attrs = [ k for k in vars(self).keys() if not k.startswith('_') ]
-        return sorted(attrs) if sortedP==True else attrs
+        if sortedP==True:
+            return sorted(attrs)
+        else:
+            return attrs
 
     def attr_get_aslist(self, at):                                                        # return value, if its singleton Treacl instance, return as a one-item list
         atv = getattr(self, at)
-        return atv if isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]) else [atv]
+        if isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]):
+            return atv
+        else:
+            return [atv]
 
     # Treacl "user" properties                                                            # as an alternative to attributes in the dunder .__dict__
                                                                                           # see README explanation
@@ -73,6 +68,7 @@ class Treacl(object):
 
     depthIndent      =    4                                                               # number of spaces indent for tree printing
     valPrintMaxWidth =   40                                                               # for pformat # width is max horizontal number of characters, e.g when printing a list
+    ppMaxDepth       = 1000                                                               # default maximum depth for indented printing/exporting of treacl trees or graphs
 
     def pformat_indented(self, v, indent=0):
         '''use pprint pformat but then add additional indent for given depth'''           # pprint module inserts one less whitespace for first line
@@ -87,16 +83,18 @@ class Treacl(object):
             for atv in self.attr_get_aslist(at):                                          # deeper nested lists are not checked
                 for s in self.pformat_indented(atv, len(nameStr)): print(s, file=file)    # use pretty print to print python base datatype
 
-    def pptree(self, depth=0, sortedP=False, file=sys.stdout):
+                                                                                          # TBD: add justShow = type|size|subnodes|·..
+    def pptree(self, depth=0, sortedP=False, file=sys.stdout, maxDepth=ppMaxDepth):
         '''print treacl tree recursively'''
         print(file=file)                                                                  # TBD: if singleton, don't print a CRLF
-        for at in self.attrs_list(sortedP=sortedP):                                       # same as self.__dict__:
-            print(nameStr := ' ' * self.depthIndent * depth + f'{at}: ', end='', file=file)
-            for atv in self.attr_get_aslist(at):                                          # deeper nested lists are not checked
-                if isinstance(atv, Treacl):
-                    atv.pptree(depth + 1, file=file)                                      # recurse
-                else:
-                    for s in self.pformat_indented(atv, len(nameStr)): print(s, file=file)# use pretty print to print python base datatype
+        if depth<maxDepth:
+            for at in self.attrs_list(sortedP=sortedP):                                   # same as self.__dict__:
+                print(nameStr := ' ' * self.depthIndent * depth + f'{at}: ', end='', file=file)
+                for atv in self.attr_get_aslist(at):                                      # deeper nested lists are not checked
+                    if isinstance(atv, Treacl):
+                        atv.pptree(depth + 1, file=file, maxDepth=maxDepth)               # recurse
+                    else:
+                        for s in self.pformat_indented(atv, len(nameStr)): print(s, file=file)# use pretty print to print python base datatype
 
     def tree_paths_to_list(self, varName=""):                                             # list all paths in tree
         '''generate all paths to a given depth'''
@@ -161,11 +159,28 @@ class Treacl(object):
                             if isinstance(e, Treacl): resLst += e.tree_find_paths_pathex(pathCdr, lpth)  # recurse
         return resLst
 
-    # def tree_find_paths_pathexex # TBD extended path-expressions
+    def tree_to_json(self, depth=0, file=sys.stdout, maxDepth=ppMaxDepth):
+        '''generate json version of the treacl struture
+           delegating to json.dumps() where possible'''
+        if depth<maxDepth:
+            print("{", file=file)
+            for ai,at in enumerate(atL := self.attrs_list()):                                 # same as self.__dict__:
+                print(nameStr := (' ' * self.depthIndent * depth) + f' "{at}": ', end='', file=file)
+                for atv in (atvl := self.attr_get_aslist(at)):                                # deeper nested lists are not checked
+                    if isinstance(atv, Treacl): atv.tree_to_json(depth + 1, file=file, maxDepth=maxDepth)         # recurse
+                    else:
+                        try:    print(json.dumps(atv, indent=self.depthIndent * (depth+1)), file=file, end='')        # use a to_json method if the datatype has one?
+                        except: print(f'"{type(atv)}"', file=file, end='')
+                if ai!=len(atL)-1: print(",", file=file)                                      # in json, no comma allowed after last item in dict or list
+            print('}', file=file, end='')
+        else:
+            print('"elided..."')
+        if depth==0: print()
 
+    # def tree_find_paths_pathexex # TBD extended path-expressions
     # graph functions
 
-    def ppgraph(self, depth=0, occurDict={}, sortedP=False):
+    def ppgraph(self, depth=0, occurDict={}, sortedP=False, maxDepth=ppMaxDepth):
         '''print treacl graph recursively'''                                              # print(' ' * self.depthIndent * depth+self.getProp("name"))
         occurDict[self] = True                                                            # TBD: by changing this to a counter, one could cycle up to a limit
         for at in self.attrs_list(sortedP=sortedP):                                       # TBD: if singleton, don't print a CRLF
@@ -173,7 +188,7 @@ class Treacl(object):
             print(nameStr := ' ' * self.depthIndent * depth + f'{at}: ', end='')
             for atv in self.attr_get_aslist(at):                                          # deeper nested lists are not checked
                 if isinstance(atv, Treacl):
-                    if atv not in occurDict: atv.ppgraph(depth + 1, occurDict=occurDict)  # recurse
+                    if atv not in occurDict: atv.ppgraph(depth + 1, occurDict=occurDict, maxDepth=maxDepth)  # recurse
                 else:
                     for s in self.pformat_indented(atv, len(nameStr)): print(s, end='')   # use pretty print to print python base datatype
 
@@ -217,61 +232,11 @@ class Treacl(object):
         lineRePat = re.compile(regexPattrn, re.I)
         return [ p for p in self.graph_paths_to_list() if lineRePat.search(p) ]
 
-    def graph_copy():  # create a copy of just the treacl tree
-        pass
-
     def graph_save(self, fPath):
         '''super simple - save treacl graph/tree and all attached data using pickle'''
         with open(fPath,'wb') as f: pickle.dump(self, f)
 
-    def graph_save_selective(self, fDirPath, saveDispatch={}, occurDict={}):
-        '''save treacl in pickle, but any types in leaves are
-           saved in neigbouring file with unique id, using type-based dispatch
-           saveDispatch = {'pandas.dataframe': 'pd.save_to_csv'}
-        '''
-        occurDict[self] = True
-        resLst = []
-        saveObjs = {obj:None, place:}  # same object and where it's kept                  # keep list of objects to save via dispatch
-        for at in self.attrs_list():
-            resLst += [pth := f'{varName}.{at}']
-            atv = getattr(self, at)
-            if isinstance(atv, Treacl):
-                if atv not in occurDict: resLst += atv.graph_paths_to_list(pth, occurDict)# recurse
-            elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]):     # n.b. more deeply nested lists are not checked
-                for ei,e in enumerate(atv):
-                    resLst += [lpth := f'{varName}.{at}[{ei}]']
-                    if isinstance(e, Treacl):
-                        if e not in occurDict: resLst += e.graph_paths_to_list(lpth, occurDict) # recurse
-
-        for o in saveObjs:                                                               # save object in it's own file, name with id
-            unique_id = gensym()
-            apply(saveDispatch(type(o)), o, filestem=unique_id)
-
-        with open("fDirPath",'wb') as f:
-            pickle.dump(cfg, f)
-
-        self.pickle
-        return resLst
-
-
-def treacl_graph_load(fileDirPath):
-    '''load treacl in pickle, patch in any types in leaves that
-       have been saved in neigbouring file with unique id
-       not a method since there are not
-        treacl objects are create
-       thoug
-    '''
-    occurDict[self] = True
-    resLst = []
-    for at in self.attrs_list():
-        resLst += [pth := f'{varName}.{at}']
-        atv = getattr(self, at)
-        if isinstance(atv, Treacl):
-            if atv not in occurDict: resLst += atv.graph_paths_to_list(pth, occurDict)# recurse
-        elif isinstance(atv, list) and any([isinstance(e, Treacl) for e in atv]):     # n.b. more deeply nested lists are not checked
-            for ei,e in enumerate(atv):
-                resLst += [lpth := f'{varName}.{at}[{ei}]']
-                if isinstance(e, Treacl):
-                    if e not in occurDict: resLst += e.graph_paths_to_list(lpth, occurDict) # recurse
-    return resLst
+def treacl_load(fPath):                                                                   # not a class method since it creates treacl instances
+    '''super simple - laod treacl graph/tree and all attached data using pickle'''
+    with open(fPath, 'rb') as fb: return pickle.load(fb)
 
